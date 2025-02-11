58244c8f3db77a417a91dbb9b72b61454294e52c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-11 10:36:52 +0530

    home screen

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,8 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+    );
   }
 }
