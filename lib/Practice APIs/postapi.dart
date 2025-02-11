2b441cd2dffba39092471a94b47c351522ccd7e0
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-11 23:27:50 +0530

    calling api using GET

---
+++ lib/Practice APIs/postapi.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Postapi extends StatefulWidget {
+  const Postapi({super.key});
+
+  @override
+  State<Postapi> createState() => _PostapiState();
+}
+
+class _PostapiState extends State<Postapi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
