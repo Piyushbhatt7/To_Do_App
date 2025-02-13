359f038963beef4a19bef36f760e6841a979a059
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-13 11:09:24 +0530

    userApi class to call user info

--- lib/services/user_api.dart
+++ lib/services/user_api.dart
@@ -1,3 +1,42 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
 class UserApi {
 
+
+  Future<void> fetchUsers() async
+  {
+    print('Fetchusers called');
+    const url = 'https://randomuser.me/api/?results=50';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+    final body = response.body;
+    final json = jsonDecode(body);
+    final results = json['results'] as List<dynamic>;
+    final transform = results.map((e) {
+
+      final name = UserName(
+        last: e['name']['title'],
+        title: e['name']['first'],
+        first: e['name']['last'],
+
+      );
+      return User(
+        cell: e['cell'],
+        email: e['email'],
+        phone: e['phone'],
+        gender: e['gender'],
+        nat: e['nat'],
+        picture: e['picture']['large'],
+        name: name,
+
+      );
+    }).toList();
+    setState(() {
+      users = transform;
+    });
+    print('fetch user completed');
+  }
+
+}
 }
