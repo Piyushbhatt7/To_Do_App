1c5abfa6ecb57e17fb22f810daf552f0b65a6ec1
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-14 22:36:10 +0530

    userApi class to call user info

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -45,7 +45,7 @@ class _HomeScreenState extends State<HomeScreen> {
          // ),
            title: Text(user.fullName),
            tileColor: color,
-          subtitle: Text(user.location.country),
+          subtitle: Text(user.location.street),
           // subtitle: Text(user.total.toString()),
            //subtitle: Text(name.toString()),
           leading: CircleAvatar(
