f25a82174b92671575549dbe2dfef4d516bd8f88
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-15 11:21:06 +0530

    creating user class Picture

--- lib/models/user_picture.dart
+++ lib/models/user_picture.dart
@@ -3,4 +3,10 @@ class UserPicture {
   final String large;
   final String medium;
   final String small;
+  UserPicture({
+
+    required this.large,
+    required this.medium,
+    required this.small
+})
 }
