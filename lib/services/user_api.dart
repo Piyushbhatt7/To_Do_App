d78100f862a199ec20e1d4ee730a7b1a9a61d17e
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-15 11:11:36 +0530

    userApi class to call user info

--- lib/services/user_api.dart
+++ lib/services/user_api.dart
@@ -1,10 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:to_do/models/user_dob.dart';
-import 'package:to_do/models/user_location.dart';
-
 import '../models/user.dart';
-import '../models/user_name.dart';
+
 
 class UserApi {
 
