109a487138376804943904b1c8928a4603be902a
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-15 22:52:59 +0530

    creating user class Picture

--- lib/adding_page.dart
+++ lib/adding_page.dart
@@ -10,6 +10,6 @@ class AddingTodoPage extends StatefulWidget {
 class _AddingTodoPageState extends State<AddingTodoPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold();
   }
 }
