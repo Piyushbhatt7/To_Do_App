e00c14f5faaa8976200f638b4b1a2684222a932c
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-16 14:12:59 +0530

    making a complete ToDo Application

--- lib/adding_page.dart
+++ lib/adding_page.dart
@@ -65,7 +65,7 @@ class _AddingTodoPageState extends State<AddingTodoPage> {
     // Submit data to the server // 13:20
      final url = 'https://api.nstack.in/v1/todos';
      final uri = Uri.parse(url);
-    http.post(url)
+     final response = await http.post(url);
     // Show success or fail message based on status
 
 
