7cb01330ab4ad08676205e559ab50d85e6570f2d
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-16 17:53:58 +0530

    making a complete ToDo Application

--- lib/adding_page.dart
+++ lib/adding_page.dart
@@ -85,7 +85,7 @@ class _AddingTodoPageState extends State<AddingTodoPage> {
     if(response.statusCode == 201)
       {
         print('Success');
-        showSuccessMessage('');
+        showSuccessMessage('creation success');
       }
 
     else {
