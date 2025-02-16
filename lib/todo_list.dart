d803f4f2b996605d4d0b25cc280e686e9752ed79
Piyushbhatt7 <vengersmon003@gmail.com>
2025-02-16 18:55:36 +0530

    making a complete ToDo Application

--- lib/todo_list.dart
+++ lib/todo_list.dart
@@ -46,7 +46,9 @@ class _TodoListState extends State<TodoList> {
     final url =  'https: //api.nstack.in/v1/todos?page=1&limit=10';
     final uri = Uri.parse(url);
     final response = await http.get(uri);
-    print(response.statusCode);
+    if(response.statusCode == 200){
+
+    }
     print(response.body);
   }
 }
